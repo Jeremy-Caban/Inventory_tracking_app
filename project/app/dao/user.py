a47440941c540be1fc7f6fefcab2b2e4dcc05277
leamsi <leamsi@Ubuntu22.04.myguest.virtualbox.org>
2023-11-18 17:09:41 -0400

    Added User DAO

---
+++ project/app/dao/user.py
@@ -0,0 +1,111 @@
+from app.config import dbconfig
+import psycopg2
+
+
+# Leamsi working here
+
+class UserDAO:
+
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            user=dbconfig.user,
+            password=dbconfig.password,
+            host=dbconfig.host,
+            dbname=dbconfig.dbname,
+            port=dbconfig.port)
+        print(self.conn)
+
+    def getAllUsers(self):
+        cursor = self.conn.cursor()
+        query = "select * from user;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserById(self, uid):
+        cursor = self.conn.cursor()
+        query = "select * from user where uid = %s;"
+        cursor.execute(query, (uid,))
+        result = cursor.fetchone()
+        return result
+
+    def getUserByFirstName(self, fname):
+        cursor = self.conn.cursor()
+        query = "select * from user where fname = %s;"
+        cursor.execute(query, (fname,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserByLastName(self, lname):
+        cursor = self.conn.cursor()
+        query = "select * from user where lname = %s;"
+        cursor.execute(query, (lname,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserByLastName(self, lname):
+        cursor = self.conn.cursor()
+        query = "select * from user where lname = %s;"
+        cursor.execute(query, (lname,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserByFullName(self, fname, lname):
+        cursor = self.conn.cursor()
+        query = "select * from user where fname = %s and lname = %s;"
+        cursor.execute(query, (fname, lname))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserByEmail(self, uemail):
+        cursor = self.conn.cursor()
+        query = "select * from user where uemail = %s;"
+        cursor.execute(query, (uemail,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserByPhone(self, uphone):
+        cursor = self.conn.cursor()
+        query = "select * from user where uphone = %s;"
+        cursor.execute(query, (uphone,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def insert(self, fname, lname, uemail=None, uphone=None):
+        cursor = self.conn.cursor()
+        query = '''
+                insert into user(fname, lname, uemail, uphone)
+                values (%s, %s, %s, %s) returning uid;
+                '''
+        cursor.execute(query, (fname, lname, uemail, uphone))
+        sid = cursor.fetchone()[0]
+        self.conn.commit()
+        return uid
+
+    def update(self, pid, fname, lname, uemail, uphone):
+        cursor = self.conn.cursor()
+        query = "update user set fname = %s, lname = %s, uemail = %s, uphone = %s where uid = %s;"
+        cursor.execute(query, (fname, lname, uemail, uphone, pid))
+        self.conn.commit()
+        return uid
+
+    def delete(self, uid):
+        cursor = self.conn.cursor()
+        query = "delete from user where uid = %s;"
+        cursor.execute(query, (uid,))
+        self.conn.commit()
+        return uid
