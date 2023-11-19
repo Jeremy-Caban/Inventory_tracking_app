1abb496fc880a5536ce081188ef0127ec78032dc
XV1R <xavytron@gmail.com>
2023-11-19 19:39:18 -0400

    Fix mispelled variable name

--- project/app/handlers/transaction.py
+++ project/app/handlers/transaction.py
@@ -306,7 +306,7 @@ class TransactionHandler:
 
         #update tables
         warehouse_dao = WarehouseDAO()
-        new_budet = warehouse_dao.get_warehouse_budget(wid) + ttotal
+        new_budget = warehouse_dao.get_warehouse_budget(wid) + ttotal
         wid = warehouse_dao.set_warehouse_budget(wid, new_budget)
 
         new_quantity = curr_quantity - tquantity
