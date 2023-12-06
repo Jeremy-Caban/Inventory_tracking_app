e6c3104c5688d0322f32756af68214ef9a4d2042
ace7806 <red7806@gmail.com>
2023-12-06 00:40:29 -0400

    slight changes

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: python3 main.py
+worker: voila --enable_nbextensions=True --Voila.ip=0.0.0.0 --port=$PORT --no-browser frontend.ipynb
--- main.py
+++ main.py
@@ -8,4 +8,3 @@ port = int(os.environ.get("PORT", 5000))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)
-    subprocess.run(["voila", "--enable_nbextensions=True", "--port=$PORT", "--no-browser", "frontend.ipynb"])
