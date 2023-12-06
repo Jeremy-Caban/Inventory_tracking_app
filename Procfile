ca8262effae5822c4d1d76a51e4deae1c7b3009b
ace7806 <red7806@gmail.com>
2023-12-06 00:32:12 -0400

    deploy frontend via subprocess

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: python3 main.py
-web: voila --enable_nbextensions=True --port=$PORT --no-browser frontend.ipynb
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from app import app
 import os
+import subprocess
+
+
+
 port = int(os.environ.get("PORT", 5000))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)
+    subprocess.run(["voila", "--enable_nbextensions=True", "--port=$PORT", "--no-browser", "frontend.ipynb"])
