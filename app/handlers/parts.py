aa66aa44d9d0feb1fb3e9cb9082f518b4c3dacce
ace7806 <red7806@gmail.com>
2024-01-06 00:39:19 -0400

    fixed validation for updating parts

--- app/handlers/parts.py
+++ app/handlers/parts.py
@@ -92,31 +92,29 @@ class PartHandler:
             return jsonify(DeleteStatus="OK"), 200
         return jsonify(Error="Part not found."), 404
 
-    def update_part(self, pid, form):
+    def update_part(self, pid, json):
         #assume that part types/names can have numbers in them
         #something like red40
         dao = PartsDAO()
         if not dao.getPartById(pid):
             return jsonify(Error="Part not found."), 404
-        pprice = form.get('pprice')
-        pname = form.get('pname')
-        ptype = form.get('ptype')
-        if pprice is None:
-            return jsonify(Error = 'Part price not provided.')
+        pprice = json.get('pprice')
+        pname = json.get('pname')
+        ptype = json.get('ptype')
+        if not pprice:
+            return jsonify(Error = 'Part price not provided.'), 400
         if pname is None or len(pname) == 0:
-            return jsonify(Error = 'Part name not provided.')
+            return jsonify(Error = 'Part name not provided.'), 400
         if ptype is None or len(ptype) == 0:
-            return jsonify(Error = 'Part type not provided.')
+            return jsonify(Error = 'Part type not provided.'), 400
         if len(pname) > 100 or len(ptype) > 100:
-            return jsonify(Error = 'Part name/type too long.')
-        if not isinstance(pprice, (int,float)) and not pprice.isnumeric():
-            return jsonify(Error = 'Part price not valid.')
+            return jsonify(Error = 'Part name/type too long.'), 400
+        if not isinstance(pprice, (int,float)) or pprice <=0:
+            return jsonify(Error = 'Part price not valid.'), 400
         if not isinstance(ptype, str) or not ptype.isascii():
-            return jsonify(Error = 'Part type not valid.')
+            return jsonify(Error = 'Part type not valid.'), 400
         if not isinstance(pname, str) or not ptype.isascii():
-            return jsonify(Error = 'Part name not valid.')
-        if float(pprice) <= 0:
-            return jsonify(Error = 'Part price cannot be 0.')
+            return jsonify(Error = 'Part name not valid.'), 400
         dao.update(pid, pprice, ptype, pname)
         result = self.build_part_attributes(pid, pprice, ptype, pname)
         return jsonify(Part=result), 200
